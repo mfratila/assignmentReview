aad04b3d00d1d195296e4bfc3a1577c04e773fc9
Mihai Fratila <mihai.fratila01@gmail.com>
2023-11-28 12:50:52 +0200

    add name property to the assignments

--- src/main/java/com/mfratila/assignmentSubmission/domain/Assignment.java
+++ src/main/java/com/mfratila/assignmentSubmission/domain/Assignment.java
@@ -12,6 +12,7 @@ public class Assignment {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	private String name;
 	private String status;
 	private String githubUrl;
 	private String branch;
@@ -25,6 +26,12 @@ public class Assignment {
 	}
 	public void setId(Long id) {
 		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
 	}
 	public String getStatus() {
 		return status;
