ba0facf6ee1600ce6ccc68bd972c21e2c3acdcce
Mihai <mihai.fratila01@gmail.com>
2024-04-06 22:17:22 +0300

    implement email notification when new comments are added to assignments

--- src/main/java/com/mfratila/assignmentSubmission/service/CommentService.java
+++ src/main/java/com/mfratila/assignmentSubmission/service/CommentService.java
@@ -4,8 +4,10 @@ import com.mfratila.assignmentSubmission.domain.Assignment;
 import com.mfratila.assignmentSubmission.domain.Comment;
 import com.mfratila.assignmentSubmission.domain.User;
 import com.mfratila.assignmentSubmission.dto.CommentDto;
+import com.mfratila.assignmentSubmission.mail.EmailSenderService;
 import com.mfratila.assignmentSubmission.repository.AssignmentRepository;
 import com.mfratila.assignmentSubmission.repository.CommentRepository;
+import com.mfratila.assignmentSubmission.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +21,10 @@ public class CommentService {
     private CommentRepository commentRepository;
     @Autowired
     private AssignmentRepository assignmentRepository;
+    @Autowired
+    private EmailSenderService emailSenderService;
+    @Autowired
+    private JwtUtil jwtUtil;
 
     public Comment save(CommentDto commentDto, User user) {
         Comment comment = new Comment();
@@ -35,13 +41,18 @@ public class CommentService {
             comment.setCreatedDate(commentDto.getCreatedDate());
         }
 
+        emailSenderService.sendMail(assignment.getUser().getUsername(),
+                "New comment on Assignment " + assignment.getNumber(),
+                "The user " + jwtUtil.getUsernameFromToken(commentDto.getUser())
+                        + " has left the following comment on your assignment: \n \n"
+                        + commentDto.getText());
+
         return commentRepository.save(comment);
     }
 
     public Set<Comment> getCommentsByAssignmentId(Long assignmentId) {
-        Set<Comment> comments = commentRepository.findByAssignmentId(assignmentId);
 
-        return comments;
+        return commentRepository.findByAssignmentId(assignmentId);
     }
 
     public void deleteCommentById(Long commentId) {
