86c75394448839f257e8c0ea37ec8ab8dc37c180
Mihai Fratila <mihai.fratila01@gmail.com>
2024-05-12 22:17:11 +0300

    add package for performance testing

--- front-end/wdio.conf.js
+++ front-end/wdio.conf.js
@@ -21,7 +21,7 @@ exports.config = {
     // of the config file unless it's absolute.
     //
     specs: [
-        './test/specs/**/*.js'
+        './test/specs/**.js'
     ],
     // Patterns to exclude.
     exclude: [
@@ -50,7 +50,7 @@ exports.config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
     }],
 
     //
@@ -100,7 +100,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['visual'],
+    services: ['visual', 'devtools'],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
