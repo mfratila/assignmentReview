c19ecf82603422a0e5c78f973954b09fa1d4fbf2
Mihai Fratila <mihai.fratila01@gmail.com>
2024-05-26 22:34:20 +0300

    update query

--- src/main/java/com/mfratila/assignmentSubmission/repository/AssignmentRepository.java
+++ src/main/java/com/mfratila/assignmentSubmission/repository/AssignmentRepository.java
@@ -12,7 +12,7 @@ public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
     Set<Assignment> findByUser(User user);
 
     @Query("select a from Assignment a "
-            + "where (a.status = 'submitted' and (a.codeReviewer is null or a.codeReviewer = :codeReviewer ))"
+            + "where (a.status = 'Trimis' and (a.codeReviewer is null or a.codeReviewer = :codeReviewer ))"
             + "or a.codeReviewer = :codeReviewer")
     Set<Assignment> findByCodeReviewer(User codeReviewer);
 
