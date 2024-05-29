590a1b28efa4e9ba5411f3a346755e2f8801084b
Mihai <mihai.fratila01@gmail.com>
2024-05-29 22:09:03 +0300

    add page title

--- front-end/src/CourseListView/index.js
+++ front-end/src/CourseListView/index.js
@@ -9,7 +9,13 @@ function CourseListView() {
     return (
         <>
             <NavigationBar />
-            <Container style={{marginTop: '2em'}}>
+            <Container style={{ marginTop: '2em' }}>
+                <Row className="mb-4">
+                    <Col className="text-center">
+                        <h1 id="page-title">Listă Materiale Didactice</h1>
+                        <hr />
+                    </Col>
+                </Row>
                 <Row>
                     {data.cardsInfo.map((card, index) => (
                         <Col className='mb-4' key={index} sm={12} md={6} lg={4} xl={3}>
